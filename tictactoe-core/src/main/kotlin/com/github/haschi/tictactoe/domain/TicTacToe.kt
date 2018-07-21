ce1e0ffc8c6cdf9031a68d8e57cbc7e623409d58
Matthias Haschla <matthias.haschka@gmail.com>
2018-07-21 14:02:39 +0200

    Refactoring

--- tictactoe-core/src/main/kotlin/com/github/haschi/tictactoe/domain/TicTacToe.kt
+++ tictactoe-core/src/main/kotlin/com/github/haschi/tictactoe/domain/TicTacToe.kt
@@ -23,13 +23,6 @@ class TicTacToe
     @AggregateIdentifier
     private lateinit var id: Aggregatkennung
 
-    val besetzteFelder: List<Feld>
-        get() = spielverlauf.map { it.feld }
-
-    val letzterSpieler: Spieler
-           get() = spielverlauf.map { it.spieler }
-                   .lastOrNull() ?: Spieler.keiner
-
     private var spielverlauf = listOf<Spielzug>()
 
     constructor()
@@ -63,7 +56,14 @@ class TicTacToe
 
     private fun fallsSpielerGewinnt(spielzug: Spielzug, dann: (Spielzug) -> Unit)
     {
-        if (wirdSpielerGewinnen(spielzug))
+        val gewinn = listOf(Feld('A', 1), Feld('B', 2), Feld('C', 3))
+        val gewinn2 = listOf(Feld('B', 1), Feld('B', 2), Feld('B', 3))
+        val gewinne = listOf(gewinn, gewinn2)
+        val felderDesSpieler = (spielverlauf + spielzug)
+                .filter { it.spieler == spielzug.spieler }
+                .map { it.feld }
+        if (gewinne.map { felderDesSpieler.containsAll(it) }
+                .contains(true))
         {
             dann(spielzug)
         }
@@ -71,7 +71,8 @@ class TicTacToe
 
     private fun fallsSpielerNichtAnDerReiheIst(spielzug: Spielzug, dann: (Spielzug) -> Unit)
     {
-        if (letzterSpieler == spielzug.spieler)
+        if (spielverlauf.map { it.spieler }
+                .lastOrNull() ?: Spieler.keiner == spielzug.spieler)
         {
             dann(spielzug)
         }
@@ -79,26 +80,10 @@ class TicTacToe
 
     private fun fallsFeldBelegt(spielzug: Spielzug, dann: (Spielzug) -> Unit)
     {
-        if (besetzteFelder.contains(spielzug.feld))
+        if (spielverlauf.map { it.feld }.contains(spielzug.feld))
         {
             dann(spielzug)
         }
-    }
-
-    private fun wirdSpielerGewinnen(spielzug: Spielzug): Boolean
-    {
-        val gewinn = listOf(Feld('A', 1), Feld('B', 2), Feld('C', 3))
-
-        val gewinn2 = listOf(Feld('B', 1), Feld('B', 2), Feld('B', 3))
-
-        val gewinne = listOf(gewinn, gewinn2)
-
-        val felderDesSpieler = (spielverlauf + spielzug)
-                .filter { it.spieler == spielzug.spieler }
-                .map { it.feld }
-
-        return gewinne.map { felderDesSpieler.containsAll(it) }
-                .contains(true)
     }
 
     @EventSourcingHandler
