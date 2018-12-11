2dd7731d95619c593ec77fbc2e2f4aa6b0ed75a0
Matthias Haschla <matthias.haschka@gmail.com>
2018-12-11 12:35:17 +0100

    Tests mit H2 DB

--- tictactoe-test/src/test/kotlin/requirements/backend/testing/Infrastructure.kt
+++ tictactoe-test/src/test/kotlin/requirements/backend/testing/Infrastructure.kt
@@ -6,14 +6,30 @@ import com.github.haschi.tictactoe.application.TicTacToeGateway
 import com.github.haschi.tictactoe.application.WarteraumGateway
 import org.axonframework.commandhandling.CommandBus
 import org.axonframework.commandhandling.gateway.CommandGatewayFactory
+import org.axonframework.common.jpa.SimpleEntityManagerProvider
+import org.axonframework.common.transaction.TransactionManager
+import org.axonframework.config.ConfigurationScopeAwareProvider
+import org.axonframework.deadline.DeadlineManager
+import org.axonframework.deadline.SimpleDeadlineManager
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore
+import org.axonframework.eventsourcing.eventstore.EventStore
+import org.axonframework.eventsourcing.eventstore.jpa.DomainEventEntry
+import org.axonframework.eventsourcing.eventstore.jpa.JpaEventStorageEngine
+import org.axonframework.modelling.saga.repository.jpa.SagaEntry
 import org.axonframework.serialization.Serializer
 import org.axonframework.serialization.json.JacksonSerializer
+import org.axonframework.spring.config.AxonConfiguration
+import org.axonframework.spring.messaging.unitofwork.SpringTransactionManager
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
-import org.springframework.stereotype.Service
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+import javax.persistence.EntityManager
 
-@Service
+@Configuration
+@EntityScan(basePackageClasses = [DomainEventEntry::class, SagaEntry::class])
 class Infrastructure(@Autowired private val mapper: ObjectMapper) {
 
     @Bean
@@ -52,5 +68,29 @@ class Infrastructure(@Autowired private val mapper: ObjectMapper) {
         return JacksonSerializer.builder()
             .objectMapper(mapper)
             .build()
+    }
+
+    @Bean
+    fun deadlineManager(configuration: AxonConfiguration, tm: TransactionManager): DeadlineManager {
+        return SimpleDeadlineManager.builder()
+            .scopeAwareProvider(ConfigurationScopeAwareProvider(configuration))
+            .transactionManager(tm)
+            .build()
+    }
+
+    @Bean
+    fun eventStore(
+        configuration: AxonConfiguration,
+        entityManager: EntityManager,
+        transactionManager: PlatformTransactionManager
+    ): EventStore {
+
+        val storage = JpaEventStorageEngine.builder()
+            .entityManagerProvider(SimpleEntityManagerProvider(entityManager))
+            .transactionManager(SpringTransactionManager(transactionManager))
+
+            .build()
+
+        return EmbeddedEventStore.builder().storageEngine(storage).build()
     }
 }
--- tictactoe-test/src/test/kotlin/requirements/core/testing/Infrastructure.kt
+++ tictactoe-test/src/test/kotlin/requirements/core/testing/Infrastructure.kt
@@ -6,20 +6,31 @@ import com.github.haschi.tictactoe.application.TicTacToeGateway
 import com.github.haschi.tictactoe.application.WarteraumGateway
 import org.axonframework.commandhandling.CommandBus
 import org.axonframework.commandhandling.gateway.CommandGatewayFactory
+import org.axonframework.common.jpa.SimpleEntityManagerProvider
 import org.axonframework.common.transaction.TransactionManager
 import org.axonframework.config.ConfigurationScopeAwareProvider
 import org.axonframework.deadline.DeadlineManager
 import org.axonframework.deadline.SimpleDeadlineManager
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore
+import org.axonframework.eventsourcing.eventstore.EventStore
+import org.axonframework.eventsourcing.eventstore.jpa.DomainEventEntry
+import org.axonframework.eventsourcing.eventstore.jpa.JpaEventStorageEngine
+import org.axonframework.modelling.saga.repository.jpa.SagaEntry
 import org.axonframework.serialization.Serializer
 import org.axonframework.serialization.json.JacksonSerializer
 import org.axonframework.spring.config.AxonConfiguration
+import org.axonframework.spring.messaging.unitofwork.SpringTransactionManager
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
-import org.springframework.stereotype.Service
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+import javax.persistence.EntityManager
 
 
-@Service
+@Configuration
+@EntityScan(basePackageClasses = [DomainEventEntry::class, SagaEntry::class])
 class Infrastructure(@Autowired private val mapper: ObjectMapper) {
     @Bean
     fun commandGatewayFactory(commandBus: CommandBus): CommandGatewayFactory {
@@ -67,10 +78,21 @@ class Infrastructure(@Autowired private val mapper: ObjectMapper) {
             .build()
     }
 
-//    @Bean
-//    fun eventBus(configuration: AxonConfiguration): EventBus {
-//        return configuration.eventBus()
-//    }
+    @Bean
+    fun eventStore(
+        configuration: AxonConfiguration,
+        entityManager: EntityManager,
+        transactionManager: PlatformTransactionManager
+    ): EventStore {
+
+        val storage = JpaEventStorageEngine.builder()
+            .entityManagerProvider(SimpleEntityManagerProvider(entityManager))
+            .transactionManager(SpringTransactionManager(transactionManager))
+
+            .build()
+
+        return EmbeddedEventStore.builder().storageEngine(storage).build()
+    }
 
 //    val axonServerConfiguration = AxonServerAutoConfiguration
 //    @Bean fun eventBus(): EventBus {
