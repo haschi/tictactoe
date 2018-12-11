b8d63bd6296f87958e0df913b33405515b4c98c2
Matthias Haschla <matthias.haschka@gmail.com>
2018-12-11 15:09:33 +0100

    Update Cucumber 4

--- tictactoe-test/src/test/kotlin/requirements/backend/BackendIT.kt
+++ tictactoe-test/src/test/kotlin/requirements/backend/BackendIT.kt
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
 @CucumberOptions(
-    glue = ["com.github.haschi.tictactoe.requirements.shared", "com.github.haschi.tictactoe.requirements.backend"],
+    extraGlue = ["com.github.haschi.tictactoe.requirements.shared"],
     features = ["classpath:com/github/haschi/tictactoe/requirements"],
     plugin = ["progress"],
     tags = ["@backend"],
--- tictactoe-test/src/test/kotlin/requirements/core/CoreIT.kt
+++ tictactoe-test/src/test/kotlin/requirements/core/CoreIT.kt
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
 @CucumberOptions(
-    glue = ["com.github.haschi.tictactoe.requirements.shared", "com.github.haschi.tictactoe.requirements.core"],
+    extraGlue = ["com.github.haschi.tictactoe.requirements.shared"],
     features = ["classpath:com/github/haschi/tictactoe/requirements"],
     plugin = ["progress"],
     tags = ["@core"],
